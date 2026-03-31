8d4450d9edea92aa8e2c3b868b87ba5c4af16bde
hathanhhoangmoi <hathanhhoang0711@gmail.com>
2026-03-31 20:57:22 +0700

    Update models.py

--- models.py
+++ models.py
@@ -37,6 +37,7 @@ class ExternalOrderTracking(Base):
     order_code = Column(String, primary_key=True)
     cod_amount = Column(Integer, default=0)
     status = Column(String, default="unknown")
+    is_paid = Column(Integer, default=0)
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
 
@@ -44,6 +45,5 @@ class ExternalOrderConfig(Base):
     __tablename__ = "external_order_config"
 
     id = Column(Integer, primary_key=True, default=1)
-    fee_amount = Column(Integer, default=0)
-    fee_content = Column(Text)
+    fee_items_json = Column(Text)
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
